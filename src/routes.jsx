fe798c53d20b366515df89063e247c9c614c9178
Christine-June <gathonichris12@gmail.com>
2025-04-20 21:55:56 +0300

    Create Movie App using basic routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Movie from "./pages/Movie.jsx";
+import Directors from "./pages/Directors.jsx";
+import Actors from "./pages/Actors.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
-
-const routes = [
-   
-  ];
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+]);
 
 export default routes;
